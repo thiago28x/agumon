c85c73c3774f39998ebad2e7376d0764fa59aada
Thiago Woroszczuk <thiago_woro@outlook.com>
2023-01-18 20:22:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')()
+const PORT = 8080;
+
+app.listen(
+    PORT, () => console.log(`tá vivo o servidor em http://localhost:${PORT}`)
+)
+
+
+app.get('/teste', (req, res) => {
+    res.status(200).send({
+        produto: "camiseta",
+        tamanho: "grande"
+    })
+})
