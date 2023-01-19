dd4babca3877453bdd5a95b16f20cd752df09e72
Thiago Woroszczuk <thiago_woro@outlook.com>
2023-01-18 23:02:16 -0300

    dfgdfg

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ app.listen(
 app.get('/teste', (req, res) => {
     res.status(200).send({
         produto: "camiseta",
-        tamanho: "grande"
+        tamanho: "grande",
+        "title": "iPhone 9",
+      "description": "An apple mobile which is nothing like apple",
+      "price": 549,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones"
     })
 })
 
